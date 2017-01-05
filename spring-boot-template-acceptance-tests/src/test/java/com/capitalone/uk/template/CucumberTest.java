9efa4575d95f308911b8cd7c846a40a2d1a3566e
Julijanas Jezov <julijanas.jezov@capitalone.com>
2017-01-05 15:02:26 +0000

    refactored, more features, template

--- spring-boot-template-acceptance-tests/src/test/java/com/capitalone/uk/template/CucumberTest.java
+++ spring-boot-template-acceptance-tests/src/test/java/com/capitalone/uk/template/CucumberTest.java
@@ -1,15 +1,12 @@
 package com.capitalone.uk.template;
 
-import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.runner.RunWith;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(format = {"html:target/CucumberResults/html/cucumber-html-report",
-    "json:target/CucumberResults/json/cucumber-reports.json"},
+@CucumberOptions(plugin = {"pretty", "html:build/cucumber"},
     features = "src/test/resources/features",
     glue = "com.capitalone.uk.template")
 public class CucumberTest {
---
+++ spring-boot-template-web/src/main/java/com/capitalone/uk/template/models/ErrorResponse.java
@@ -0,0 +1,19 @@
+package com.capitalone.uk.template.models;
+
+
+public class ErrorResponse {
+
+  private String error;
+
+  public ErrorResponse(String error) {
+    this.error = error;
+  }
+
+  public String getError() {
+    return error;
+  }
+
+  public void setError(String error) {
+    this.error = error;
+  }
+}
